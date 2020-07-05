5bc1acb0a7b2cedca1eb771c39bc8df6a0275901
Huseyn <huseyinsahinduran@gmail.com>
2020-07-05 19:19:38 +0100

    4. commit masterdan

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -14,7 +14,8 @@ public class ClassOne {
 		
 		System.out.println("remote master branch");
 		System.out.println("remote master branch 1. commit");
-
+		System.out.println("israrla burada masterdan conflict");
+		
 		
 		
 	}
